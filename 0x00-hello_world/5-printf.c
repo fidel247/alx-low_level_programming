987f03a891003a36b4efbb81792310189b0db01d
otunu odey <fidelisotunnu@gmail.com>
2022-07-04 14:31:19 -0700

    commiting size

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+
 int main(void);
 {
-prinf("\"with proper grammar, but the outcome is a piece of art,\n");
+	prinf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
