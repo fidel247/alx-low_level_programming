7ff477ac980b91eef2ca55b092fa694a88566d21
otunu odey <fidelisotunnu@gmail.com>
2022-07-04 14:57:48 -0700

    commiting size of type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,11 +4,11 @@
  * Main- prints the size of various type
  * Return: 0 if exited properly, non-zero otherwise
  */
-int main(void);
+int main(void)
 {
-	printf("size of a char: %d byte(s)\n", sizeof(char));
-	printf("size of an int: %d byte(s)\n", sizeof(int));
-	printf("size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("size of a float: %d byte(s)\n", sizeof(float));
+	printf("size of a char: %ld byte(s)\n", sizeof(char));
+	printf("size of an int: %ld byte(s)\n", sizeof(int));
+	printf("size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
