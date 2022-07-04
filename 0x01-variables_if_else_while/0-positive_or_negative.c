ded87d0d130871cf71c221d9f63c2e1a62ec79f0
otunu odey <fidelisotunnu@gmail.com>
2022-07-04 05:14:07 -0700

    commiting positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
-*main -> assign a random number to the variable n each time it is executed and print |
-*based on a condition
-*Return : always 0
+ *main -> assign a random number to the variable n each time it is executed and  print
+ *based on a condition
+ *Return : always 0
 */
 int main(void)
 {
@@ -14,12 +14,12 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n >  0)
-printf("%d is positive\n", n);
+	printf("%d is positive\n", n);
 if (n == 0)
-printf("%d is zero\n", n);
+	printf("%d is zero\n", n);
 if (n < 0)
 {
-printf("%d is negative\n", n);
+	printf("%d is negative\n", n);
 }
 return (0);
 }
