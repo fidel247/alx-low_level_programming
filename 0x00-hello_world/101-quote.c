0796c98c72afbdd4c02d1dd32646846a5b1b22e3
otunu odey <fidelisotunnu@gmail.com>
2022-07-04 15:57:38 -0700

    commiting quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
+/**
+ * main - prints axactly "and that piece of art is useful"
+ * - Dora Korpar, 20-15-19
+ *   followed by a neww line
+ *   Return: always 0
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" -Dora Korpar, 20-15-19\n", 59);
+	return (1);
+}
