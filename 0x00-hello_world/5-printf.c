787fdd10aaeb97ec521aa435279d5598b2f17a45
otunu odey <fidelisotunnu@gmail.com>
2022-07-04 14:45:34 -0700

    commiting size

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * main - prints with proper grammar, but the outcome is a piece of art
+ * Return: alway 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
