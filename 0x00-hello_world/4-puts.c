af45210f4c717d57ad9181f6f28fa830f847a3ed
otunu odey <fidelisotunnu@gmail.com>
2022-07-04 16:13:29 -0700

    commiting puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - prints exactly Programming is like building a multilingual puzzle
+ *  followed by a new line
+ *  Return: aways 0
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
