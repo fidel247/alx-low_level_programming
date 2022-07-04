c1447b12e52093c49d96f0ed8debc9cd868a9a84
otunu odey <fidelisotunnu@gmail.com>
2022-07-04 05:32:58 -0700

    commiting positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main -> assign a random number to the variable n each time it is executed and  print
  * based on a condition
- * Return : always 0
+ * Return - always 0
 */
 int main(void)
 {
