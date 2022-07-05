ba3d609e695dc5e4ca0521019763d888379be734
otunu odey <fidelisotunnu@gmail.com>
2022-07-04 17:23:10 -0700

    commiting last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,10 +13,10 @@ int main(void)
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("last digit of %d is n", n);
+printf("last digit of %d is" , n);
 	if (n > 5)
 	{
-		printf("greater than 5");
+		printf(" greater than 5");
 	}
 	if (n == 0)
 	{
