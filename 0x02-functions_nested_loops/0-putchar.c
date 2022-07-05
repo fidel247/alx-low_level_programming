84097ba51ae10f5f6c9f901559a08d67b22c38e0
otunu odey <fidelisotunnu@gmail.com>
2022-07-05 05:29:49 -0700

    commiting _putchar code

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-	_purchar('_');
-	_purchar('p');
-	_purchar('u');
-	_purchar('t');
-	_purchar('c');
-	_purchar('h');
-	_purchar('a');
-	_purchar('r');
-	_purchar('\n');
+	_purchar( '_' );
+	_purchar( 'p' );
+	_purchar( 'u' );
+	_purchar( 't' );
+	_purchar( 'c' );
+	_purchar( 'h' );
+	_purchar( 'a' );
+	_purchar( 'r' );
+	_purchar( '\n' );
 	return (0);
 }
