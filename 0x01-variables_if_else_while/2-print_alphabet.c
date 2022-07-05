c4738dfda110ac88fa8ab95d2ec086f2367e2f0e
otunu odey <fidelisotunnu@gmail.com>
2022-07-04 18:06:08 -0700

    commiting print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -  prints the alphabet in lowercase
+ *  followed by a new line
+ *  Return: always 0
+ */
+int main(void)
+{
+	char
+	'for'("char = 97, char<= 122, char++")
+	{
+		putchar(char);
+	}
+	putchar(10);
+	return (0);
+}
