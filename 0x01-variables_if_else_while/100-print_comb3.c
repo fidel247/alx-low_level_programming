4c313c3d96624e43155b929a38e324fd4e2a0a52
otunu odey <fidelisotunnu@gmail.com>
2022-07-07 17:08:13 -0700

    hot fix'

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,19 +2,27 @@
 
 /**
  * main - entry point
- * Reurn: always 0
+ * Return: always 0
  */
 int main(void)
 {
 	int i;
+	int j;
 
-	for (i = 10; i < 20; i++)
+	for (i = 10; i <= 19; i++)
+	{
+	for (j = 10; j <= 19; j++)
+	{
+	if ((j % 10) > (i % 10))
 	{
 		putchar((i % 10) + '0');
-	if (i != 19)
+		putchar((j % 10) + '0');
+	if (i != 18 || j != 19)
 	{
 		putchar(',');
 		putchar(' ');
+	}
+	}
 	}
 	}
 	putchar('\n');
