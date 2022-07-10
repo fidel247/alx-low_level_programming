c147137e7f0cf42a18b1a00a5e23c5daafd9736d
otunu odey <fidelisotunnu@gmail.com>
2022-07-10 04:59:53 -0700

    commiting diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_diagonal -printing line
+ * @n: integer
+ */
+void print_diagonal(int n)
+{
+	int x;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (i = 0; i < x; i++)
+
+		}
+	}
+}
