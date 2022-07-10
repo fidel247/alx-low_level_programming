0430db0dac0efd3be40ce38ce558eb735c5d90bb
otunu odey <fidelisotunnu@gmail.com>
2022-07-10 08:02:43 -0700

    commiting 7

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,7 +5,7 @@
  */
 print_square(int size)
 {
-	int x, y;
+	int x, i;
 
 	if (size <= 0)
 		_putchar('/n');
@@ -13,11 +13,11 @@ print_square(int size)
 	{
 		for (x = 0; x < size; x++)
 		{
-		for (j = 0; y < size; y++)
-		{
-			_putchar(35);
-		}
+		for (i = 0; i < size; i++)
+			_putchar(' ');
 		_putchar('/n');
+		_putchar(92);
+		_putchat('/n');
 		}
 	}
 }
