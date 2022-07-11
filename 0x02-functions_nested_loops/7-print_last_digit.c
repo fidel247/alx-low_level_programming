f513a49691ec91f0b52c484d2ab8805bbbea714a
otunu odey <fidelisotunnu@gmail.com>
2022-07-11 01:08:32 -0700

    hot fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - prints the last digit
- * @last_digit: the argument passed
+ * @n: the argument passed
  * Return: the last digit
  */
 int print_last_digit(int n)
@@ -11,6 +11,6 @@ int print_last_digit(int n)
 
 	if (last_digit < 0)
 		last_digit = -1;
-	_putchar(las_digit + '0');
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
