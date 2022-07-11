8f97c2a788713e444cacc688f738cb1dc72230c2
otunu odey <fidelisotunnu@gmail.com>
2022-07-11 08:03:40 -0700

    commiting add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two integer
+ * @num1: the first integer to be added
+ * @num2: the seconf integer to be added
+ * Return: the result of the addition
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
