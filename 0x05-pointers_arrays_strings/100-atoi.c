3a13fabf4d324538c40c3691340aa6876180e85c
otunu odey <fidelisotunnu@gmail.com>
2022-07-12 13:54:52 -0700

    hot fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,11 +12,15 @@ int _atoi(char *s)
 
 	do {
 		if (*s == '-')
+
 			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
 			num = (num * 10) + (*s - '0');
+
 		else if (num > 0)
-		       break;
+			break;
+
 	} while (*s++);
-	return (num * sign);	
+
+	return (num * sign);
 }
