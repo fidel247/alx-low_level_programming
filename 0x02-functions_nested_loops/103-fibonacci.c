056be252a82a06c8c680b1402db2f9b3fb122197
otunu odey <fidelisotunnu@gmail.com>
2022-07-13 04:55:27 -0700

    correcting fib 2

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the value of even fib sequence
+ * not exceeding 4,000,000
+ * Returnn: always 0.
+ */
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, sumfib;
+	float tot_sum;
+
+	while (1)
+	{
+		sumfib = fib1 + fib2;
+		if (sumfib > 4000000)
+			break;
+		if ((sumfib % 2) == 0)
+			tot_sum += sumfib;
+		fib1 = fib2;
+		fib2 = sumfib;
+	}
+	printf("%.0f\n", tot_sum);
+	return (0);
+}
