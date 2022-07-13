55c447ed8d66d0f391df1790eb57210abb140c99
otunu odey <fidelisotunnu@gmail.com>
2022-07-13 03:10:25 -0700

    fib  commit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,13 +10,13 @@ int main(void)
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
 
-	for (count = 0; count < 51; count++)
+	for (count = 0; count < 50; count++)
 	{
 		sum = fib1 + fib2;
 		printf("%lu", sum);
 		fib1 = fib2;
 		fib2 = sum;
-		if (count == 50)
+		if (count == 49)
 			printf("\n");
 		else
 			printf(",");
