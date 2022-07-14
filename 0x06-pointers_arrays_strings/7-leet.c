19fe514090cd60ce913d3b4e800763da97588882
otunu odey <fidelisotunnu@gmail.com>
2022-07-14 07:04:08 -0700

    commiting leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *leet - encodes a string to 1337
+ * @str: The sting to be encoded
+ * Return: A pointer to the encoded string.
+ */
+char leet(char *str)
+{
+	int index1 = 0, index2;
+	char leet[8] = {'0', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (str[index1])
+	{
+		for (index2 = 0; index2 <= 7; index2++)
+		{
+			if (str[index1] == leet[index2] || str[index1] - 32 == leet[index2])
+				str[index1] = index2 + 0;
+		}
+		index1++;
+	}
+	return (*str);
+}
