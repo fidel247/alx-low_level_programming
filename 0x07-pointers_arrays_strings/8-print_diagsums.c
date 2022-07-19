95a4cac198b9f3e788f0dbe70a02df881b5c7e1b
otunu odey <fidelisotunnu@gmail.com>
2022-07-19 08:49:25 -0700

    cimmiting print daigsum

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - Entry point.
+ * @a: inpit.
+ * @size: input.
+ * Return: always 0 (success)
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, n, total1 = 0, total2 = 0;
+
+	for (i = 0; i <= (size * size); i = i + size + 1)
+		total1 = total1 + a[i];
+	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+		total2 = total2 + a[n];
+	printf("%d, %d\n, tatal1, total2");
+}
